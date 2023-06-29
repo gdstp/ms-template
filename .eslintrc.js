170f7ec6102f80c45d5c3a1ed9f6346bd81798e3
Gabriel Teles <gabriel.teles.pinto@gmail.com>
2023-06-29 17:17:56 -0300

    chore: add eslint and prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  plugins: ['@typescript-eslint', 'eslint-plugin-import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    'sort-imports': [
+      'error',
+      { ignoreCase: true, ignoreDeclarationSort: true }
+    ],
+    'import/order': [
+      1,
+      {
+        groups: [
+          'external',
+          'builtin',
+          'internal',
+          'sibling',
+          'parent',
+          'index'
+        ]
+      }
+    ]
+  }
+}
--- src/framework/functions/backofficeProxy.ts
+++ src/framework/functions/backofficeProxy.ts
@@ -1,16 +1,13 @@
-import { APIGatewayProxyEvent, Context } from 'aws-lambda';
-import { HttpMethods, RoutesMapper, backofficeHandler } from 'ms-common';
+import { APIGatewayProxyEvent, Context } from 'aws-lambda'
+import { backofficeHandler, HttpMethods, RoutesMapper } from 'ms-common'
 
 const routesMapper: RoutesMapper[] = [
   {
     path: '/ping',
     methods: [HttpMethods.GET],
-    function: async (
-      event: APIGatewayProxyEvent,
-      context: Context
-    ): Promise<any> => console.log('ok'),
-  },
-];
+    function: async (): Promise<any> => console.log('ok')
+  }
+]
 
 export const handler = async (event: APIGatewayProxyEvent, context: Context) =>
-  backofficeHandler(event, context, routesMapper);
+  backofficeHandler(event, context, routesMapper)
