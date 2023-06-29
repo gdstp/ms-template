84f86c5027d41cb0d0703c7e5badc59f9682881f
Gabriel Teles <gabriel.teles.pinto@gmail.com>
2023-06-29 17:45:52 -0300

    fix: remove config files

--- __tests__/config.test.ts
+++
@@ -1,7 +0,0 @@
-import { configTest } from '@/config'
-
-test('Should run jest', () => {
-  const run = configTest()
-
-  expect(run).toBe('ok')
-})
--- src/config.ts
+++
@@ -1,3 +0,0 @@
-export const configTest = () => {
-  return 'ok'
-}
