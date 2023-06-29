832a49301c7feec533bcd031b1ea37b44218e850
Gabriel Teles <gabriel.teles.pinto@gmail.com>
2023-06-29 13:02:04 -0300

    fix: import

--- src/framework/functions/backofficeProxy.ts
+++ src/framework/functions/backofficeProxy.ts
@@ -1,38 +1,16 @@
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import { HttpMethods, RoutesMapper, backofficeHandler } from 'ms-common';
 
-const routesMapper = [
+const routesMapper: RoutesMapper[] = [
   {
     path: '/ping',
-    methods: ['GET'],
+    methods: [HttpMethods.GET],
     function: async (
       event: APIGatewayProxyEvent,
       context: Context
     ): Promise<any> => console.log('ok'),
   },
 ];
-
-const backofficeHandler = async (
-  event: APIGatewayProxyEvent,
-  context: Context,
-  routesMapper: Array<{
-    path: string;
-    methods: Array<string>;
-    function: (event: APIGatewayProxyEvent, context: Context) => Promise<any>;
-  }>
-) => {
-  const executeWithoutAtt = routesMapper.find(
-    (route) => route.path === event.pathParameters?.proxy
-  );
-
-  if (executeWithoutAtt) {
-    return executeWithoutAtt.function(event, context);
-  }
-
-  return {
-    statusCode: 404,
-    body: JSON.stringify({ error: 'Route not found' }),
-  };
-};
 
 export const handler = async (event: APIGatewayProxyEvent, context: Context) =>
   backofficeHandler(event, context, routesMapper);
